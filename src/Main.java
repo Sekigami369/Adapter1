ce421ba465fec8e747ab239ef7bac262ca8c5222
sekigmai369 <skgmyum@gmail.com>
2023-08-05 21:29:37 +0900

    java GoF Adapter

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Print p = new PrintBanner("hello");
+        p.printWeek();
+        p.printStrong();
+    }  
+}
